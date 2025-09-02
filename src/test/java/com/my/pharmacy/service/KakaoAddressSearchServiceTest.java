6257b128b2ac92aa2d9cb8f219b0f19f12e50f6f
MINSsss96 <minsue0422@naver.com>
2025-09-02 14:43:17 +0900

    KAKAO_API 카테고리 검색 처리

--- src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
+++ src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
@@ -8,8 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+//KAKAO_REST_API_KEY=17e3bc9f05683e6f50c936f2c696293e
 @SpringBootTest
 class KakaoAddressSearchServiceTest {
+    @Autowired
+    KakaoCategorySearchService kakaoCategorySearchService;
+
     @Autowired
     KakaoAddressSearchService kakaoAddressSearchService;
 
@@ -24,6 +29,17 @@ class KakaoAddressSearchServiceTest {
     void uriTest(){
         KakaoApiResponseDto dto = new KakaoApiResponseDto();
         dto = kakaoAddressSearchService.requestAddressSearch("강남대로 405");
+        System.out.println(dto);
+    }
+
+    @Test
+    @DisplayName("Search Category Test")
+    void categoryTest(){
+        KakaoApiResponseDto dto = new KakaoApiResponseDto();
+        double x = 127.026692446306;
+        double y = 37.4987750083767;
+        double radius = 1000;
+        dto = kakaoCategorySearchService.resultCategorySearch(x,y,radius);
         System.out.println(dto);
     }
 }
